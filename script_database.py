4622a70b28e2e523da7eb086c1ff580a5cb090c2
Tato_Warthon <125580475+warthon-190399@users.noreply.github.com>
2023-12-19 12:55:06 -0500

    Update script_database.py

--- script_database.py
+++ script_database.py
@@ -1,13 +1,21 @@
+#1.- Importamos librerias
+
 import pandas as pd
 import networkx as nx
 from collections import Counter
 import plotly.graph_objects as go
 import plotly.express as px
 import streamlit as st
+import numpy as np
+import community
+
+#2.- Fijamos una semilla y importamos la base de datos
+
+random_seed = 42
 
 all_holds = pd.read_csv("all_holds.csv")
 
-# 3.- Creamos la SN
+#3.- Creamos la SN
 
 G = nx.from_pandas_edgelist(all_holds, "Holder", "comp", edge_attr=True)
 
@@ -83,21 +91,21 @@ degrees = G.degree()
 
 degrees_counter = Counter(dict(degrees))
 
-top_nodes = degrees_counter.most_common(10)  # cambiamos a 10 el número deseado de nodos principales
-
-print("Nodos más grandes segun numero de grados:")
-for node, degree in top_nodes:
-    print(f"Nodo: {node}, Grado: {degree}")
+top_nodes = degrees_counter.most_common(10)  #Necesitamos solo los 10 mas grandes
 
 nodes, degrees = zip(*top_nodes)
 
 df = pd.DataFrame({'Nodo': nodes, 'Grado': degrees})
 
-
-
 # Graficar con Plotly Express
-fig_degree = px.bar(df, x='Nodo', y='Grado', title='Top 10 Nodos por Grado', labels={'Nodo': 'Nodo', 'Grado': 'Grado'},
-             template="plotly_dark", opacity = 0.6)
+fig_degree = px.bar(df,
+                    x='Nodo',
+                    y='Grado',
+                    title='Top 10 Nodos por Grado',
+                    labels={'Nodo': 'Nodo', 'Grado': 'Grado'},
+                    template="plotly_dark",
+                    opacity = 0.5
+                    )
 fig_degree.update_layout(xaxis={'categoryorder': 'total descending'})
 
 #5.- Centralidad de Intermediación
@@ -142,11 +150,17 @@ df = pd.DataFrame({'Nodo': nodes, 'Centralidad de Intermediación': centralities
 df = df[::-1]
 
 # Graficar con Plotly Express
-fig_ci = px.bar(df, x='Centralidad de Intermediación', y='Nodo', orientation='h',
-             title='Top 10 Nodos por Centralidad de Intermediación',
-             labels={'Nodo': 'Nodo', 'Centralidad de Intermediación': 'Centralidad de Intermediación'},
-             template="plotly_dark",
-             color='Centralidad de Intermediación', color_continuous_scale='Redor', opacity = 0.6)
+fig_ci = px.bar(df,
+                x='Centralidad de Intermediación',
+                y='Nodo',
+                orientation='h',
+                title='Top 10 Nodos por Centralidad de Intermediación',
+                labels={'Nodo': 'Nodo', 'Centralidad de Intermediación': 'Centralidad de Intermediación'},
+                template="plotly_dark",
+                color='Centralidad de Intermediación',
+                color_continuous_scale='Redor',
+                opacity = 0.4
+                )
 
 #6.- Centralidad de Cercanía
 
@@ -225,48 +239,207 @@ fig_closeness = px.bar(df_centrality,
                         opacity=0.7
                         )
 
+# Diametro de la red
+diameter = nx.diameter(G)
+
+# Radio de la red
+radius = nx.radius(G)
+
+# Coeficiente de asortatividad:
+asort = nx.degree_assortativity_coefficient(G)
+
+# Bridges, Hubs y outliers
+
+bridges = list(nx.bridges(G))
+hubs = [node for node, degree in G.degree() if degree > 2 * (len(G) - 1) / len(G)] #medida que indica nodos más grandes que la media
+outliers = [node for node, degree in G.degree() if degree == 1] #fijamos nodos minimos como aquellos con 1 grado
+
+max_length = max(len(bridges), len(hubs), len(outliers)) #fijamos una sola longitud (la mas grande)
+
+bridges += [None] * (max_length - len(bridges))
+hubs += [None] * (max_length - len(hubs))
+outliers += [None] * (max_length - len(outliers))
+
+#consolidamos el dataframe
+
+df_roles = pd.DataFrame({
+    'Bridges':bridges,
+    'Hubs':hubs,
+    'Outliers':outliers
+})
+
+# Analisis de comunidades
+
+# Obtén las comunidades utilizando el algoritmo de Louvain
+partition = community.best_partition(G, random_state=np.random.RandomState(random_seed)) #usamos la semilla para evitar diferentes comunidades
+
+# Asignar colores a nodos según la comunidad a la que pertenecen
+node_colors = [partition[node] for node in G.nodes()]
+
+
+# Crear un nuevo gráfico con colores de comunidad
+fig_community = go.Figure(data=[
+        edge_trace,
+        go.Scatter(
+            x=node_x,
+            y=node_y,
+            mode="markers",
+            hoverinfo="text",
+            marker=dict(
+                color=node_colors,
+                showscale=True,
+                colorscale="Viridis",
+                size=node_size,
+                colorbar=dict(
+                    thickness=15,
+                    title="Comunidad",
+                    xanchor="left",
+                    titleside="right"
+                )
+            ),
+            text=list(G.nodes())  # Asegúrate de incluir el texto para el hover
+        )
+    ],
+    layout=go.Layout(
+        showlegend=False,
+        hovermode="closest",
+        margin=dict(b=0, l=0, r=0, t=0),
+        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)
+    )
+)
+
 
 #Layout
 
 logo_image = "logo.png"
 st.sidebar.image(logo_image, width=280)
+st.sidebar.write("Las opciones seleccionadas aparecerán en la parte inferior de la red principal.")
 st.sidebar.title("Seleccione indicadores:")
 
-indicadores = ["Grados de la Red", "Centralidad de Intermediación", "Centralidad de Cercanía",
-               "Centralidad de Vecindad", "Diámetro de la Red", "Radio de la Red", "Coeficiente de Asortatividad"]
+indicadores = ["Diámetro de la Red", "Radio de la Red", "Coeficiente de Asortatividad" ,"Grados de la Red", "Centralidad de Intermediación", "Centralidad de Cercanía",
+               "Centralidad de Vecindad"]
 
 indicadores_selec = st.sidebar.multiselect("Indicadores:",indicadores)
 
-st.title("¿Quién es el dueño del Dow Jones? Holi xd")
+st.sidebar.title("Análisis de Roles en la Red")
+roles = ["Bridges", "Hubs", "Outliers"]
+
+roles_selec = st.sidebar.multiselect("Seleccione el Rol:", roles)
+
+st.sidebar.title("Detección de Comunidades")
+
+detec_graf = ["Comunidades detectadas en la red"]
+detec = st.sidebar.multiselect("Ver red con detección de comunidades:", detec_graf)
+
+selected_communities = st.sidebar.selectbox("Selecciona la comunidad:", sorted(set(partition.values())))
+# Aseguramos que sea una lista
+if not isinstance(selected_communities, list):
+    selected_communities = [selected_communities]
+
+# Filtrar nodos por comunidades seleccionadas
+selected_nodes = [node for node, comm_id in partition.items() if comm_id in selected_communities]
+
+st.title("¿Quién es el dueño del Dow Jones?")
+st.write("El Dow Jones uno de los índices bursátiles más reconocidos y seguidos en el mundo financiero. Fue creado por Charles Dow en 1896 y originalmente incluía solo 12 compañías. Hoy en día, el Dow Jones Industrial Average consta de 30 grandes empresas estadounidenses líderes en diversos sectores, lo que lo convierte en un indicador representativo de la salud general del mercado de valores de Estados Unidos.")
 st.header("Red Social del Dow Jones")
-st.write("Un red social es un estructura que sirve para representar entidades **(nodos)** y relaciones **(ejes, aristas)**."
-         " En el contexto financiero sirve para entender y visualizar relaciones de inversión y la propiedad de activos de las empresas."
-         " Dentro del análisis de redes podemos identificar nodos clave (grandes propietarios), detección de comunidades y la importancia de nodos específcos en la red.")
+st.write("Los tenedores institucionales, que son inversionistas significativos en las 30 empresas del índice, ejercen influencia en decisiones de inversión y estrategias del mercado. Sus transacciones a gran escala pueden impactar los precios de acciones. En el contexto del Dow Jones, una red social representa relaciones de inversión y propiedad de activos, permitiendo identificar nodos clave (grandes propietarios), detectar comunidades y evaluar la importancia de nodos específicos en la red, facilitando el análisis de estructuras financieras complejas.")
+st.write("Debo aclarar que en la parte inferior se visualiza un gráfico interactivo de la red. Usted puede hacer zoom en las zonas de interés utilizando las opciones de selección en la parte superior derecha del gráfico.")
 st.plotly_chart(fig_sn)
-st.write("En la red podemos observar los nodos centrales que constituyen una **posición dominante** como **Vanguard**, **Blackrock Inc** o **State Street**."
-         " Esto indica la existencia de relaciones muy sólidas con otros tenedores institucionales. Sus decisiones influyen en las tendencias del mercado y afectan carteras de inversión de otros tenedores."
-         )
-st.write("El segundo grupo que ocupa una **posición intermedía** en la red comprende empresas como **Bank of America**, **Northern Trust**, **FMR** y **JP Morgan**."
-         " Al estar conectados con nodos más grandes y más pequeños, este grupo puede desempeñar un papel crucial en la **transmisión de información** dentro de la red."
-         " Probablemente actuen como **intermediarios** que facilitan la conexión entre diferentes partes del mercado financiero." 
-         )
+st.write("En la red, nodos centrales como Vanguard, BlackRock y State Street ejercen una influencia dominante, afectando las tendencias del mercado. Un grupo intermedio, con empresas como Bank of America y JP Morgan, actúa como intermediario en la transmisión de información. Otro grupo, con impacto moderado, incluye a empresas como Apple y American Express, mientras que un cuarto grupo tiene un impacto marginal, posiblemente especializándose en sectores específicos. Estas posiciones reflejan la complejidad y diversidad de roles en la red financiera.")
 st.divider()
 
-if "Grados de la Red" in indicadores_selec:
-    st.plotly_chart(fig_degree)
-    st.divider()
-
-if "Centralidad de Intermediación" in indicadores_selec:
-    st.plotly_chart(fig_ci)
-    st.divider()
-if "Centralidad de Cercanía" in indicadores_selec:
-    st.plotly_chart(fig_cc)
-    st.divider()
-
 if "Centralidad de Vecindad" in indicadores_selec:
+    st.subheader("Centralidad de Vecindad")
+    st.write("La **Centralidad de Vecindad** se entiende como la relevancia de actores financieros que ocupan **posiciones estratégicas dentro de grupos o comunidades específicas** en una red. Estos nodos pueden tener una influencia significativa en la toma de decisiones y la transmisión de información dentro de su entorno cercano, aunque no necesariamente sean los actores más grandes de la red en su totalidad.")
     st.plotly_chart(fig_closeness)
     st.divider()
 
+if "Centralidad de Cercanía" in indicadores_selec:
+    st.subheader("Centralidad de Cercanía")
+    st.write("La **Centralidad de Cercanía** indica la **importancia de nodos financieros que están en una posición cercana a otros nodos en la red**. Estos actores tienden a tener conexiones directas con una variedad de participantes en el mercado, lo que les **otorga acceso a información diversa** y les permite influir en la transmisión efectiva de datos en la red.")
+    st.plotly_chart(fig_cc)
+    st.divider()
+
+if "Centralidad de Intermediación" in indicadores_selec:
+    st.subheader("Centralidad de Intermediación")
+    st.write("La **Centralidad de Intermediación** refiere a la posición estratégica de actores financieros que actúan como **intermediarios clave** entre nodos más grandes y más pequeños en una red financiera. Estos actores facilitan la transmisión eficiente de información y desempeñan un papel fundamental en la conectividad del mercado.")
+    st.plotly_chart(fig_ci)
+    st.divider()
+
+if "Grados de la Red" in indicadores_selec:
+    st.subheader("Top Grados de la Red")
+    st.write("En el siguiente gráfico se podrá ver las 10 entidades con más conexiones en la red. Dicha conexión brinda un impacto significativo en las demás entidades y en el mercado en general.")
+    st.plotly_chart(fig_degree)
+    st.divider()
+
+if "Coeficiente de Asortatividad" in indicadores_selec:
+    st.subheader("Coeficiente de Asortatividad")
+    st.write("Mide la tendencia de los nodos de la red a conectarse con otros nodos que tienen un grado similar. El valor de asortatividad **oscila entre -1 a 1**.")
+    st.write("Si el coeficiente es **positivo**, indica que existe una tendencia de **asortatividad** de los nodos a conectarse entre aquellos que tienen un grado similar. Es decir, los nodos con muchos enlaces tienden a enlazarse con otros nodos de muchos enlaces. Del mismo modo, nodos con pocos enlaces tienden a conectarse con nodos de pocos enlaces.")
+    st.write("Si el coeficiente es **negativo**, indica una tendencia hacia la **disasortatividad**. En tal caso, los nodos con muchos enlaces tienden a conectarse con nodos con pocos enlaces y viceversa.")
+    st.metric(label="Coeficiente de Asortatividad:",
+              value=asort,
+              delta="Grado de Asortatividad en la Red")
+    st.write("En este caso, como el coeficiente de asortatividad es `-0.27`, sugiere una ligera tendencia hacia la **disasortatividad**. Esto significa que en la red, los nodos con muchos enlaces tienden a conectarse con nodos que tienen pocos enlaces y viceversa, pero la tendencia no es muy fuerte.")
+    st.divider()
+
+if "Radio de la Red" in indicadores_selec:
+    st.subheader("Radio de la Red")
+    st.write("El **radio de la red** es una medida que representa la distancia promedio entre un nodo central y todos los demás nodos en la red. Indica la **proximidad promedio** de cada institución financiera en el **Dow Jones** respecto a una entidad central. Un **radio bajo** sugiere una **mayor cohesión** en la red, ya que las distancias entre los actores clave y el nodo central son relativamente cortas.")
+    st.metric(label="Radio de la Red:",
+              value=radius,
+              delta="Distancia promedio entre dos nodos más cercanos"
+              )
+    st.write("El **radio de la red es de 2**, indicando la distancia mínima entre cualquier nodo y su nodo más cercano en la red. En términos financieros, esto sugiere que **la información y la influencia pueden propagarse rápidamente en un entorno altamente interconectado**. ")
+    st.divider()
+
+if "Diámetro de la Red" in indicadores_selec:
+    st.subheader("Diámetro de la red")
+    st.write("Describe **la distancia más larga** entre dos nodos dentro de una red. Representa la longitud máxima del camino más corto entre cualquier par de instituciones financieras en el **Dow Jones Industrial Average (DJIA)**. Si el diámetro es bajo, la red está más estrechamente conectada, ya que la distancia máxima entre actores clave es relativamente corta.")
+    st.metric(label="Diámetro de la Red:",
+              value=diameter,
+              delta="Distancia máxima entre cualquier par de entidades"
+              )
+    st.write("**El valor del diámetro de la red es 4**, es decir, la distancia máxima entre cualquier par de instituciones financieras del **Dow Jones** es de **4 conexiones**. En síntesis, sugiere una mayor eficiencia en la transmisión de **eventos del mercado**, **decisiones financieras** y **cambios en las condiciones económicas**.")
+    st.divider()
+
+if "Outliers" in roles_selec:
+    st.subheader("Nodos Outliers")
+    st.write("Nodos con conexiones mínimas (Outliers):")
+    st.write(df_roles["Outliers"])
+
+if "Hubs" in roles_selec:
+    st.subheader("Nodos Centrales (Hubs)")
+    st.write("Nodos que actúan como líderes de comunidad:")
+    st.write(df_roles["Hubs"])
+    st.divider()
+
+if "Bridges" in roles_selec:
+    st.subheader("Nodos Puente (Bridges)")
+    st.write("Nodos que actúan como puentes entre partes diferentes de la red:")
+    st.write(df_roles["Bridges"])
+    st.divider()
+
+if "Comunidades detectadas en la red" in detec:
+    st.subheader("Comunidades detectadas en la Red")
+    st.write("La detección de comunidades consiste en identificar grupos de nodos en una red que están más estrechamente conectados entre sí que con el resto de la red. Estos grupos de nodos forman comunidades o subgrupos dentro de la red más amplia. El objetivo principal de este análisis es revelar la estructura interna y la organización de la red, destacando las relaciones más fuertes y significativas entre sus miembros.")
+    st.write("En el caso del Dow Jones se detectaron 5 comunidades cada una con un color distinto a modo de diferenciarlas. Usted puede visualizar en el gráfico las comunidades o a través del sidebar donde se visualizarán en formato de tablas.")
+    st.plotly_chart(fig_community)
+
+# Mostrar tabla con nodos ordenados por tamaño
+    if selected_nodes:
+        node_sizes = {node: G.degree[node] for node in selected_nodes}
+        sorted_nodes = sorted(node_sizes.items(), key=lambda x: x[1], reverse=True)
+        
+        st.subheader(f"Comunidad {selected_communities}")
+        st.table(sorted_nodes)
+    else:
+        st.sidebar.info("Selecciona al menos una comunidad en el multiselect.")
+
+if st.sidebar.button("Descarga la Base de Datos"):
+    all_holds.to_csv("all_holds.csv", index=False)
+    st.sidebar.success("¡Base de Datos descarga!")
 
 
 
